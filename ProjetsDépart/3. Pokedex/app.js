0dba540258cc6f6bc0d6101bfdaad33a5775f8ec
MarwenCode <marwen_jdidi@yahoo.com>
2023-04-20 00:35:36 +0200

    pocke desck part 2

--- ProjetsDépart/3. Pokedex/app.js
+++ ProjetsDépart/3. Pokedex/app.js
@@ -1,22 +1,49 @@
-const searchInput = document.querySelector('.recherche-poke input');
+const searchInput = document.querySelector(".recherche-poke input");
+const listePoke = document.querySelector('.liste-poke');
+
+let allPokemon = [];
+let tableauFin = [];
+
+
+
+
+const types = {
+    grass: '#78c850',
+	ground: '#E2BF65',
+	dragon: '#6F35FC',
+	fire: '#F58271',
+	electric: '#F7D02C',
+	fairy: '#D685AD',
+	poison: '#966DA3',
+	bug: '#B3F594',
+	water: '#6390F0',
+	normal: '#D9D5D8',
+	psychic: '#F95587',
+	flying: '#A98FF3',
+	fighting: '#C25956',
+    rock: '#B6A136',
+    ghost: '#735797',
+    ice: '#96D9D6'
+};
+
+
+
 
 
 const fetchPokemonBase = () => {
-    fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
+  fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
     .then((response) => response.json())
     .then((allPoke) => {
-        console.log(allPoke)
-        allPoke.results.forEach((pokemon) => {
-            fetchPokemonComplet(pokemon)
-        })
-    })
-}
+      console.log(allPoke);
+      allPoke.results.forEach((pokemon) => {
+        fetchPokemonComplet(pokemon);
+      });
+    });
+};
 
-fetchPokemonBase()
-
+fetchPokemonBase();
 
 //using async await
-
 
 // const fetchPokemonBase = async () => {
 //     try {
@@ -27,22 +54,73 @@ fetchPokemonBase()
 //       console.error(error);
 //     }
 //   }
-  
+
 //   fetchPokemonBase();
-
-
 
 //fetch all caracteristic of each Pokemon
 const fetchPokemonComplet = (pokemon) => {
+  let objPokemonFull = {};
 
-    const url = pokemon.url;
-    const name = pokemon.name
+  const url = pokemon.url;
+  const name = pokemon.name;
 
-    fetch(url)
+  fetch(url)
     .then((response) => response.json())
-    .then((pokemonData) => console.log(pokemonData))
+    .then((pokemonData) => {
+      console.log(pokemonData);
 
+      objPokemonFull.id = pokemonData.id;
+      objPokemonFull.pic = pokemonData.sprites.front_default;
+      objPokemonFull.type = pokemonData.types[0].type.name;
+
+      fetch(`https://pokeapi.co/api/v2/pokemon-species/${name}`)
+        .then((response) => response.json())
+        .then((pokeData) => {
+          console.log(pokeData);
+
+          objPokemonFull.name = pokeData.names[4].name;
+          allPokemon.push(objPokemonFull);
+
+          if (allPokemon.length === 151) {
+            console.log(allPokemon);
+
+            tableauFin = allPokemon.sort((a, b) => {
+                return a.id - b.id
+            }).slice(0,21)
+
+            console.log(tableauFin)
+
+            createdCard(tableauFin)
+
+          }
+        });
+    });
+};
+
+
+//create carte Pokemon
+const createdCard = (arr) => {
+    for(let i = 0; i < arr.length ; i++) {
+
+        const carte = document.createElement('li');
+        let couleur = types[arr[i].type];
+        carte.style.background = couleur
+        const textCarte = document.createElement('h5');
+        textCarte.innerText = arr[i].name;
+        const idCarte = document.createElement("p");
+        idCarte.innerText = `ID ${arr[i].id}`;
+        const imgCarte = document.createElement('img');
+        imgCarte.src = arr[i].pic;
+
+        carte.appendChild(imgCarte);
+        carte.appendChild(textCarte);
+        carte.appendChild(idCarte);
+
+        listePoke.appendChild(carte);
+
+
+
+
+
+    }
 }
-
-
-  
