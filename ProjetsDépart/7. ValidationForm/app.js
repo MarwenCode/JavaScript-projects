c26628228d7b4002d0101e01ec03cd712be54071
MarwenCode <marwen_jdidi@yahoo.com>
2023-04-25 23:32:23 +0200

    validated form password regex algorithm

--- ProjetsDépart/7. ValidationForm/app.js
+++ ProjetsDépart/7. ValidationForm/app.js
@@ -7,6 +7,8 @@ const allSpan = document.querySelectorAll('span');
 const allLigne = document.querySelectorAll('.ligne div');
 
 
+//user name
+
 inpUtilisateur.addEventListener('input', (e) => {
 
     if(e.target.value.lenght >=3 ) {
@@ -23,4 +25,104 @@ inpUtilisateur.addEventListener('input', (e) => {
 
 
 
+});
+
+// mail 
+
+// inpMail.addEventListener('input', (e) => {
+
+//     const regexEmail = /\S+@\S+\.\S+/;
+    
+//     if(e.target.value.search(regexEmail)  >= 0){
+
+//         allImg[1].style.display = "inline";
+//         allImg[1].src = "ressources/check.svg";
+//         allSpan[1].style.display = "none";
+
+//     } else  {
+
+//         allImg[1].style.display = "inline";
+//         allImg[1].src = "ressources/error.svg";
+//         allSpan[1].style.display = "inline";
+
+//     }
+
+// })
+
+inpMail.addEventListener('input', (e) => {
+
+    const regexEmail = /\S+@\S+\.\S+/;
+    
+    if(e.target.value.search(regexEmail) === 0){
+
+        allImg[1].style.display = "inline";
+        allImg[1].src = "ressources/check.svg";
+        allSpan[1].style.display = "none";
+
+    } else if(e.target.value.search(regexEmail) === -1) {
+
+        allImg[1].style.display = "inline";
+        allImg[1].src = "ressources/error.svg";
+        allSpan[1].style.display = "inline";
+
+    }
+
 })
+
+
+// password 
+let valeurInp;
+const specialCar = /[^a-zA-Z0-9]/;
+const alphabet = /[a-z]/i;
+const chiffres = /[0-9]/;
+
+let objValidation = {
+    symbole : 0,
+    lettre : 0,
+    chiffre : 0
+}
+
+
+inpMdp.addEventListener('input', (e) => {
+
+      valeurInp = e.target.value;
+
+    if (valeurInp.search(specialCar) !== -1) {
+        objValidation.symbole = 1 ;
+    }
+    if (valeurInp.search(alphabet) !== -1) {
+        objValidation.lettre = 1 ;
+    }
+    if (valeurInp.search(chiffres) !== -1) {
+        objValidation.chiffre = 1 ;
+    }
+
+    console.log(objValidation)
+
+
+   if(e.inputType = 'deleteContentBackward'){
+        if(valeurInp.search(specialCar) === -1){
+            objValidation.symbole = 0;
+        }
+        if(valeurInp.search(alphabet) === -1){
+            objValidation.lettre = 0;
+        }
+        if(valeurInp.search(chiffres) === -1){
+            objValidation.chiffre = 0;
+        }
+    } 
+
+    console.log(objValidation)
+
+
+
+
+
+
+
+})
+
+
+
+
+
